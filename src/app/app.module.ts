536b659465804ff56f74a476b071e3e2242d5614
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-04 15:03:53 -0600

    created custom filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { HomeComponent } from './home/home.component';
+import { routing } from './app.routing';
+import { FilterPipe } from './filter.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectoryComponent,
+    HomeComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import {DirectoryComponent} from './directory/directory.component';
+import {HomeComponent} from './home/home.component';
+import {Routes, RouterModule } from "@angular/router";
+
+
+const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'directory',
+    component: DirectoryComponent
+  }
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
